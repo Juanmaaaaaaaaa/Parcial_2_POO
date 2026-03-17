c145335ef4d60382622e3a84c02f6fcfda74fb5f
Sara Ruiz <sararuizar2006@gmail.com>
2026-03-16 21:53:17 -0500

    00000

--- clases/InventoryEntry.cs
+++ clases/InventoryEntry.cs
@@ -1,8 +1,26 @@
 ﻿using System;
 
-public class StoreInventory
+public class InventoryEntry
 {
-	public StoreInventory()
-	{
-	}
+    public Item Item { get; }
+    public int Quantity { get; private set; }
+
+    public InventoryEntry(Item item, int quantity)
+    {
+        Item = item;
+        Quantity = quantity;
+    }
+
+    public void Add(int amount)
+    {
+        Quantity += amount;
+    }
+
+    public void Remove(int amount)
+    {
+        if (amount > Quantity)
+            throw new InvalidOperationException("No hay suficiente cantidad");
+
+        Quantity -= amount;
+    }
 }
--- clases/Player.cs
+++ clases/Player.cs
@@ -2,7 +2,20 @@
 
 public class Player
 {
-	public Player()
-	{
-	}
+    public decimal Gold { get; private set; }
+    public PlayerInventory Inventory { get; }
+
+    public Player(decimal gold)
+    {
+        Gold = gold;
+        Inventory = new PlayerInventory();
+    }
+
+    public void SpendGold(decimal amount)
+    {
+        if (amount > Gold)
+            throw new InvalidOperationException("No hay suficiente oro");
+
+        Gold -= amount;
+    }
 }
